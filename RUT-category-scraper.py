70b32734e0937076e14b6cccfeeced3bd7f66ddb
ScribbleG <49068170+ScribbleGhost@users.noreply.github.com>
2020-01-19 23:06:52 +0100

    Exports results to a CSV file

--- RUT-category-scraper.py
+++ RUT-category-scraper.py
@@ -1,28 +1,34 @@
-import requests,time
+import requests,time,csv
 from bs4 import BeautifulSoup
 
 def scrape():
-	page = 0
-	page_has_data = True
 
-	while page_has_data:
-		base_url = 'https://RUT.org/forum/'
-		r = requests.get('https://RUT.org/forum/viewforum.php?f=1715&', params=dict(start=page))
-		soup = BeautifulSoup(r.text, 'html.parser')
+	with open ('results.csv', 'w', encoding='utf-8-sig', newline='') as f:
+		writer = csv.writer(f, delimiter=';')
+		
+		page = 0
+		page_has_data = True
 
-		print(f'---------Scraping Page: {page}---------', end=';', sep=';')
-		print(soup.find('title').text)
+		while page_has_data:
+			base_url = 'https://RUT.org/forum/'
+			r = requests.get('https://RUT.org/forum/viewforum.php?f=1715&', params=dict(start=page))
+			soup = BeautifulSoup(r.text, 'html.parser')
 
-		if len(soup.findAll("a", {"class": "torTopic"})) == 0:
-			page_has_data = False
-			print('---------Done Scraping---------')
+			print(f'---------Scraping Page: {page}---------', end=';', sep=';')
+			print(soup.find('title').text)
 
-		for release in soup.find_all("a", {"class": "torTopic"}):
-			print(release.text, end=';', sep=';')
-			print(base_url + release.get('href'))
+			if len(soup.findAll("a", {"class": "torTopic"})) == 0:
+				page_has_data = False
+				print('---------Done Scraping---------')
 
-		page +=50
-		time.sleep(2)
+			for release in soup.find_all("a", {"class": "torTopic"}):
+				LINK = base_url + release.get('href')
+				TITLE = release.text
+				writer.writerow([LINK,TITLE])
+
+			page +=50
+
+			time.sleep(2)
 
 if __name__ == '__main__':
 	scrape()
