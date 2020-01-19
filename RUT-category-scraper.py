0ed0bf7c162e0a7d0e4b4e075cdb5c8051aa4260
ScribbleG <49068170+ScribbleGhost@users.noreply.github.com>
2020-01-19 23:23:42 +0100

    Exports to CSV file
    Added feature to set forum sub-category using the parameters `f`` number. For example `1714`.

--- RUT-category-scraper.py
+++ RUT-category-scraper.py
@@ -6,12 +6,13 @@ def scrape():
 	with open ('results.csv', 'w', encoding='utf-8-sig', newline='') as f:
 		writer = csv.writer(f, delimiter=';')
 		
-		page = 0
-		page_has_data = True
+		page = 0 # Don't change
+		category = 1714 # Set the number of the forum category. For example: .../viewforum.php?f=1714
+		page_has_data = True # Don't change
 
 		while page_has_data:
 			base_url = 'https://RUT.org/forum/'
-			r = requests.get('https://RUT.org/forum/viewforum.php?f=1715&', params=dict(start=page))
+			r = requests.get('https://RUT.org/forum/viewforum.php?', params=dict(f=category,start=page))
 			soup = BeautifulSoup(r.text, 'html.parser')
 
 			print(f'---------Scraping Page: {page}---------', end=';', sep=';')
@@ -27,7 +28,7 @@ def scrape():
 				writer.writerow([LINK,TITLE])
 
 			page +=50
-
+			
 			time.sleep(2)
 
 if __name__ == '__main__':
